9993d8279bb101de38f99b40de289e2576f7e99f
KimAng Desktop <kimangkheang@gmail.com>
2021-09-02 22:26:04 +0700

    update key

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { useContainer } from 'class-validator';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app/app.module';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   setupSwagger(app);
